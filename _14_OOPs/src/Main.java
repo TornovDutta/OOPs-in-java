94cdc36384fad22f9490e3b7d05f7c767c168ccc
TornovDutta <tornovdutta@gmail.com>
2024-07-04 03:56:30 +0530

    add info

--- _14_OOPs/src/Main.java
+++ _14_OOPs/src/Main.java
@@ -11,5 +11,7 @@ public class Main {
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
         }
+
+        //This is OOPs in java
     }
 }
